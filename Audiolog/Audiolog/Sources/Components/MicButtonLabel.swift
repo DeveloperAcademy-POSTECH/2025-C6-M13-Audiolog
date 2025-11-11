5e27384284076ae703d26f55ae9b9beeb559e324
Sean Cho / Frank <chxhyxn@gmail.com>
2025-11-11 09:43:29 +0900

    feat: MicButtonLabel 광택이 회전해요~

--- Audiolog/Audiolog/Sources/Components/MicButtonLabel.swift
+++ Audiolog/Audiolog/Sources/Components/MicButtonLabel.swift
@@ -5,9 +5,14 @@
 //  Created by Sean Cho on 10/30/25.
 //
 
+import CoreMotion
 import SwiftUI
 
 struct MicButtonLabel: View {
+    private let motionManager = CMMotionManager()
+
+    @State private var rotateAngle: Angle = Angle(radians: 0)
+
     let isRecording: Bool
 
     var body: some View {
@@ -24,14 +29,8 @@ struct MicButtonLabel: View {
                             AngularGradient(
                                 gradient: Gradient(colors: [
                                     Color.white.opacity(1),
-                                    Color.white.opacity(1),
-                                    Color.white.opacity(1),
-                                    Color.white.opacity(0),
                                     Color.white.opacity(0),
                                     Color.white.opacity(1),
-                                    Color.white.opacity(1),
-                                    Color.white.opacity(1),
-                                    Color.white.opacity(0),
                                     Color.white.opacity(0),
                                     Color.white.opacity(1),
                                 ]),
@@ -39,12 +38,46 @@ struct MicButtonLabel: View {
                             ),
                             lineWidth: 3
                         )
-                        .blur(radius: 0.6)
+                        .blur(radius: 0.4)
                         .opacity(0.8)
                         .padding(1)
+                        .rotationEffect(rotateAngle)
                 }
             )
             .glassEffect(.regular.interactive())
+            .onAppear {
+                startMotionUpdates()
+            }
+    }
+
+    private func fetchDeviceOrientation() {
+        if let gravity = motionManager.deviceMotion?.gravity {
+            let x = gravity.x
+            let y = gravity.y
+
+            let newAngle = -Angle(radians: atan2(y, x))
+            let current = rotateAngle.radians
+            let target = newAngle.radians
+
+            var delta = target - current
+            while delta > .pi { delta -= 2 * .pi }
+            while delta <= -.pi { delta += 2 * .pi }
+
+            if abs(delta) > .pi {
+                rotateAngle = newAngle
+            } else {
+                withAnimation(.easeOut(duration: 0.1)) {
+                    rotateAngle = newAngle
+                }
+            }
+        }
+    }
+
+    private func startMotionUpdates() {
+        motionManager.deviceMotionUpdateInterval = 0.1
+        motionManager.startDeviceMotionUpdates(to: .main) { _, _ in
+            fetchDeviceOrientation()
+        }
     }
 }
 
