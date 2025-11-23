ca6ea8c98ce4b8d298a3646690dbd94cbe85b529
Sean Cho / Frank <chxhyxn@gmail.com>
2025-11-23 21:39:50 +0900

    feat: 미니플레이어 디자인 변경

--- Audiolog/Audiolog/Sources/MiniPlayerView.swift
+++ Audiolog/Audiolog/Sources/MiniPlayerView.swift
@@ -13,102 +13,93 @@ struct MiniPlayerView: View {
     @State private var isSliderEditing: Bool = false
 
     var body: some View {
-        VStack(spacing: 10) {
+        HStack(alignment: .center, spacing: 0) {
             if let current = audioPlayer.current {
-                HStack {
-                    VStack(alignment: .leading, spacing: 0) {
-                        Text(current.title == "" ? "제목 생성중" : current.title)
-                            .font(.body.weight(.semibold))
-                            .foregroundColor(.lbl1)
-                            .lineLimit(1)
+                VStack(alignment: .leading, spacing: 0) {
+                    Text(current.title == "" ? "제목 생성중" : current.title)
+                        .font(.footnote.weight(.semibold))
+                        .foregroundColor(.lbl1)
+                        .lineLimit(1)
+                        .padding(.top, 5)
 
-                        if let createdAt = audioPlayer.current?.createdAt {
-                            Text(createdAt.formatted("M월 d일 EEEE"))
-                                .font(.footnote)
-                                .foregroundColor(.lbl3)
-                        }
-                    }
-                    Spacer()
-                }
-                .padding(.top, 20)
-                .padding(.horizontal, 20)
+                    HStack(spacing: 5) {
+                        Text(formatTime(audioPlayer.currentPlaybackTime))
 
-                HStack(spacing: 5) {
-                    Text(formatTime(audioPlayer.currentPlaybackTime))
-
-                    Slider(
-                        value:
-                            Binding(
-                                get: { audioPlayer.currentPlaybackTime },
-                                set: { value in
-                                    audioPlayer.seek(to: value)
-                                }
-                            ),
-                        in: 0...audioPlayer.currentDuration,
-                    )
-                    .tint(.lbl1)
-
-                    Text(formatTime(audioPlayer.currentDuration))
-                }
-                .font(.footnote.weight(.semibold))
-                .monospacedDigit()
-                .foregroundStyle(.lbl3)
-                .padding(.horizontal, 20)
-
-                HStack(spacing: 50) {
-                    Button {
-                        audioPlayer.playPreviousInPlaylist()
-                    } label: {
-                        Image(systemName: "backward.fill")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 31, height: 31)
-                    }
-
-                    Button {
-                        audioPlayer.togglePlayPause()
-                    } label: {
-                        Image(
-                            systemName: audioPlayer.isPlaying
-                                ? "pause.fill"
-                                : "play.fill"
+                        Slider(
+                            value:
+                                Binding(
+                                    get: {
+                                        audioPlayer.currentPlaybackTime
+                                    },
+                                    set: { value in
+                                        audioPlayer.seek(to: value)
+                                    }
+                                ),
+                            in: 0...audioPlayer.currentDuration,
                         )
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 31, height: 31)
-                    }
+                        .tint(.lbl1)
 
-                    Button {
-                        audioPlayer.playNextInPlaylist()
-                    } label: {
-                        Image(systemName: "forward.fill")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 31, height: 31)
+                        Text(formatTime(audioPlayer.currentDuration))
                     }
+                    .frame(height: 24)
+                    .font(.caption.weight(.semibold))
+                    .monospacedDigit()
+                    .foregroundStyle(.lbl3)
                 }
-                .tint(.lbl1)
-                .padding(.top, 10)
-                .padding(.bottom, 20)
             } else {
-                HStack(alignment: .center) {
+                VStack(alignment: .leading, spacing: 0) {
                     Text("재생 중이 아님")
                         .font(.footnote.weight(.semibold))
                         .foregroundStyle(.lbl2)
+                        .lineLimit(1)
+                        .padding(.top, 5)
 
-                    Spacer()
+                    HStack(spacing: 5) {
+                        Text(formatTime(0))
 
-                    Image(
-                        systemName: "play.fill"
-                    )
-                    .font(.system(size: 17))
-                    .foregroundStyle(.lbl2)
+                        Capsule()
+                            .fill(.lbl2.opacity(0.5))
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 6)
+
+                        Text(formatTime(0))
+                    }
+                    .frame(height: 24)
+                    .font(.caption.weight(.semibold))
+                    .monospacedDigit()
+                    .foregroundStyle(.lbl3)
                 }
-                .padding(.horizontal, 20)
-                .frame(height: 48)
-                .frame(maxWidth: .infinity)
             }
+            Spacer()
+            Button {
+                if audioPlayer.current != nil {
+                    audioPlayer.togglePlayPause()
+                }
+            } label: {
+                Image(
+                    systemName: audioPlayer.isPlaying
+                        ? "pause.fill"
+                        : "play.fill"
+                )
+                .font(.title2)
+                .frame(width: 39, height: 44)
+            }
+            .tint(audioPlayer.current == nil ? .lbl2 : .lbl1)
+            .disabled(audioPlayer.current == nil)
+
+            Button {
+                audioPlayer.playNextInPlaylist()
+            } label: {
+                Image(systemName: "forward.fill")
+                    .font(.title2)
+                    .frame(width: 39, height: 44)
+            }
+            .tint(audioPlayer.playlist.isEmpty ? .lbl2 : .lbl1)
+            .disabled(audioPlayer.playlist.isEmpty)
         }
+        .padding(.horizontal, 20)
+        .frame(height: 68)
+        .frame(maxWidth: .infinity)
         .glassEffect(in: .rect(cornerRadius: 34))
         .animation(.default, value: audioPlayer.isPlaying)
         .accessibilityAction(.magicTap) {
